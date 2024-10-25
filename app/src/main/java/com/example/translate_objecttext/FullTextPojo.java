d1c41cef3ec390f930b28de86b7d58edb5c06282
harshsk898 <7019970898a@gmail.com>
2024-10-25 11:39:44 +0530

    Initial commit

---
+++ app/src/main/java/com/example/translate_objecttext/FullTextPojo.java
@@ -0,0 +1,17 @@
+package com.example.translate_objecttext;
+
+public class FullTextPojo {
+    private String fullText;
+
+    public FullTextPojo(String fullText) {
+        this.fullText = fullText;
+    }
+
+    public String getFullText() {
+        return fullText;
+    }
+
+    public void setFullText(String fullText) {
+        this.fullText = fullText;
+    }
+}
---
+++ app/src/main/java/com/example/translate_objecttext/ILoadImage.java
@@ -0,0 +1,7 @@
+package com.example.translate_objecttext;
+
+import android.graphics.Bitmap;
+
+public interface ILoadImage {
+    void onLoadImage(String language);
+}
